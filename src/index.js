6bfccb7581213b3bb005aafe9f4cc8e084c93ff2
Arild Wanvik Tvergrov <arildt@mesan.no>
2016-05-28 20:25:08 +0200

    Implement enum segment.

--- src/index.js
+++ src/index.js
@@ -2,15 +2,17 @@ import { javaClassSegment } from './javaClassSegment';
 import { javaConstructorSegment } from './javaConstructorSegment';
 import { javaMethodSegment } from './javaMethodSegment';
 import { javaAnnotationSegment } from './javaAnnotationSegment';
-import { javaGenericTypeSegment } from './javaGenericTypeSegment';
+import { javaEnumSegment } from './javaEnumSegment';
 import { javaStringLiteralSegment } from './javaStringLiteralSegment';
 import { javaArrayLiteralSegment } from './javaArrayLiteralSegment';
+import { javaGenericTypeSegment } from './javaGenericTypeSegment';
 
 module.exports = {
   javaClassSegment,
   javaConstructorSegment,
   javaMethodSegment,
   javaAnnotationSegment,
+  javaEnumSegment,
   javaStringLiteralSegment,
   javaArrayLiteralSegment,
   javaGenericTypeSegment
---
+++ src/javaEnumSegment.js
@@ -0,0 +1,103 @@
+import { javaAnnotationSegment } from './javaAnnotationSegment';
+import { javaConstructorSegment } from './javaConstructorSegment';
+import { javaFieldSegment } from './javaFieldSegment';
+import { javaMethodSegment } from './javaMethodSegment';
+import { javaGenericTypeSegment } from './javaGenericTypeSegment';
+import { javaAccessModifierSegment } from './javaAccessModifierSegment';
+import { interweave } from './util';
+
+function scopeSegment(scope) {
+  return scope === 'class' ? 'static ' : '';
+}
+
+function interfaceSegment({ name, genericTypes = [] }) {
+  return `${name}${javaGenericTypeSegment(genericTypes)}`;
+}
+
+function constantArgumentsSegment(args) {
+  return args.join(', ');
+}
+
+function constantWithArgumentsSegment(name, args, IS_LAST) {
+  return ({ t, indent }) =>
+    `${args.length === 1
+      ? t(`${name}(${constantArgumentsSegment(args)})`)
+      : t(`${name}(`, indent(args.join(',\n')), ')')}` +
+    `${IS_LAST ? ';' : ','}`;
+}
+
+function constantWithoutArgumentsSegment(name, IS_LAST) {
+  return `${name}${IS_LAST ? ';' : ','}`;
+}
+
+function constantSegment({ name, args = [], annotations = [] } = {}, { IS_LAST } = {}) {
+  return tplate => {
+    const { t } = tplate;
+    return t(
+      annotations.length ? annotations.map(javaAnnotationSegment) : undefined,
+      (args.length
+        ? constantWithArgumentsSegment(name, args, IS_LAST)
+        : constantWithoutArgumentsSegment(name, IS_LAST)));
+  };
+}
+
+function enumHeaderSegment(accessModifier, scope, name) {
+  return `${javaAccessModifierSegment(accessModifier)}${scopeSegment(scope)}enum ${name}`;
+}
+
+export function javaEnumSegment({
+  name = 'MyEnum',
+  accessModifier = 'public',
+  scope = 'instance',
+  interfaces = [],
+  annotations = [],
+  fields = [],
+  constants = [],
+  constructors = [],
+  methods = []
+  } = {}) {
+  const constructorsWithName = constructors.map(c =>
+    Object.assign({}, c, { name, accessModifier: 'package' }));
+
+  return ({ t, indent, map }) => t(
+    // Annotation lines
+    annotations.length ? annotations.map(javaAnnotationSegment) : undefined,
+
+    // Class Declaration
+    interfaces.length
+      ? enumHeaderSegment(accessModifier, scope, name)
+      : `${enumHeaderSegment(accessModifier, scope, name)} {`,
+
+    interfaces.length
+      ? indent(`implements ${interfaces.map(interfaceSegment).join(', ')} {`)
+      : undefined,
+
+    // Constant lines
+    constants.length ? '' : undefined,
+    constants.length
+      ? `${indent(map(constants, constantSegment))}`
+      : undefined,
+
+    // Field lines
+    fields.length ? '' : undefined,
+    fields.length ? indent(fields.map(javaFieldSegment)) : undefined,
+
+    // Constructors
+    constructors.length
+      ? ''
+      : undefined,
+    constructors.length
+      ? indent(interweave(constructorsWithName.map(javaConstructorSegment)))
+      : undefined,
+
+    // Methods
+    methods.length
+      ? ''
+      : undefined,
+    methods.length
+      ? indent(interweave(methods.map(javaMethodSegment)))
+      : undefined,
+
+    '}'
+  );
+}
